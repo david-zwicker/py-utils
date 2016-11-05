4ad6fc509aa1014bf417eb3e3e2cbf41724fb65b
David Zwicker <dzwicker@seas.harvard.edu>
2016-11-04 20:51:53 -0400

    Added a method that connects components of spatial graphs

---
+++ utils/math/graphs.py
@@ -0,0 +1,72 @@
+'''
+Created on Nov 4, 2016
+
+@author: David Zwicker <dzwicker@seas.harvard.edu>
+'''
+
+from __future__ import division
+
+import numpy as np
+from scipy.spatial import distance
+import networkx as nx
+
+
+
+def connect_components(graph, pos_key, dist_key=None):
+    """ connects all components by iteratively inserting edges between
+    components that have minimal distance.
+    
+    `graph` is a networkx graph object with positions assigned to nodes
+    `pos_key` gives the key for the node attribute that stores the position
+    `dist_key` if given, the length of the new edges are stored as edge attributes
+    """
+    
+    # build a distance matrix for all nodes
+    vertices = nx.get_node_attributes(graph, pos_key)
+    nodes = np.array(vertices.keys())
+    positions = vertices.values()
+    dists = distance.squareform(distance.pdist(positions))
+
+    # determine all subgraphs and build a list of indices into the distance matrix
+    subgraphs = list(nx.connected_component_subgraphs(graph))
+    sg_nids_list = []
+    for sg in subgraphs:
+        sg_nids = [np.flatnonzero(nodes == n)[0] for n in sg.nodes()]
+        sg_nids_list.append(sg_nids)
+    
+    # initialize result with first subgraph
+    result = subgraphs.pop(0)
+    result_nids = sg_nids_list.pop(0)
+    
+    # iterate until all subgraphs have been added
+    while subgraphs:
+        # find subgraph that is closest to `result`
+        sg_min, nid_sg, nid_res, dist_min = None, None, None, np.inf
+        for k, sg_nids in enumerate(sg_nids_list):
+            dist_mat = dists[sg_nids, :][:, result_nids]
+            x, y = np.unravel_index(dist_mat.argmin(), dist_mat.shape)
+            dist = dists[x, y]
+            if dist < dist_min:
+                sg_min = k #< index into the subgraph
+                dist_min = dist #< its distance to `result`
+                # store the node indices for the connecting edge
+                nid_sg = sg_nids[x]
+                nid_res = result_nids[y]
+
+        # add graph to `result`
+        result.add_nodes_from(subgraphs[sg_min].nodes(data=True))
+        result.add_edges_from(subgraphs[sg_min].edges(data=True))
+        
+        # add a new edge between the subgraph and `result`
+        if dist_key is not None:
+            attr_dict = {dist_key: dists[nid_res, nid_sg]}
+        else:
+            attr_dict = None
+        result.add_edge(nodes[nid_res], nodes[nid_sg], attr_dict)
+            
+        # remove the subgraph from the to-do list
+        result_nids.extend(sg_nids_list.pop(sg_min))
+        subgraphs.pop(sg_min)
+        
+    return result
+
---
+++ utils/math/tests/test_graphs.py
@@ -0,0 +1,48 @@
+'''
+Created on Nov 4, 2016
+
+@author: David Zwicker <dzwicker@seas.harvard.edu>
+'''
+
+from __future__ import division
+
+import unittest
+
+import networkx as nx
+
+from .. import graphs
+
+
+
+class TestGraphs(unittest.TestCase):
+
+    _multiprocess_can_split_ = True  # let nose know that tests can run parallel
+
+    def test_conncet_components(self):
+        g = nx.Graph()
+        
+        g.add_node(0, pos=(0, 0))
+        g.add_node(1, pos=(1, 1))
+        g.add_edges_from([[0, 1]])
+        
+        # trivial test with connected graph
+        gc = graphs.connect_components(g, 'pos', 'dist')
+        self.assertTrue(nx.is_connected(gc))
+        self.assertItemsEqual(gc.nodes(), [0, 1])
+        self.assertItemsEqual(gc.edges(), [(0, 1)])
+        
+        # add another component
+        g.add_node(10, pos=(10, 0))
+        g.add_node(11, pos=(9, 1))
+        g.add_edges_from([[10, 11]])
+        
+        gc = graphs.connect_components(g, 'pos', 'dist')
+        self.assertTrue(nx.is_connected(gc))
+        self.assertItemsEqual(gc.nodes(), [0, 1, 10, 11])
+        self.assertItemsEqual(gc.edges(), [(0, 1), (10, 11), (1, 11)])
+        self.assertEqual(nx.get_edge_attributes(gc, 'dist')[(1, 11)], 8)
+
+
+
+if __name__ == "__main__":
+    unittest.main()

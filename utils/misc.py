baf6f4208282585a79af620483bd1a9cc2c1858c
David Zwicker <dzwicker@seas.harvard.edu>
2017-01-12 21:22:58 -0800

    Added functon for formatting timedelta values

--- utils/misc.py
+++ utils/misc.py
@@ -4,11 +4,12 @@ Created on Jan 21, 2015
 @author: David Zwicker <dzwicker@seas.harvard.edu>
 '''
 
-from __future__ import division
+from __future__ import division, absolute_import
 
 import functools
 import logging
 import itertools
+import math
 import sys
 import timeit
 import types
@@ -226,3 +227,53 @@ def iter_pairwise(iterable, circular=False):
     if circular:
         b = itertools.chain(b, [first_item])
     return zip(a, b)
+
+
+def format_timedelta(value, time_format=None):
+    """ formats a datetime.timedelta with the given format.
+    Code copied from Django as explained in
+    http://stackoverflow.com/a/30339105/932593
+    """ 
+    
+    if time_format is None:
+        time_format = "{days} days, {hours2}:{minutes2}:{seconds2}"
+
+    if hasattr(value, 'seconds'):
+        seconds = value.seconds + value.days * 24 * 3600
+    else:
+        seconds = int(value)
+
+    seconds_total = seconds
+
+    minutes = int(math.floor(seconds / 60))
+    minutes_total = minutes
+    seconds -= minutes * 60
+
+    hours = int(math.floor(minutes / 60))
+    hours_total = hours
+    minutes -= hours * 60
+
+    days = int(math.floor(hours / 24))
+    days_total = days
+    hours -= days * 24
+
+    years = int(math.floor(days / 365))
+    years_total = years
+    days -= years * 365
+
+    return time_format.format(**{
+        'seconds': seconds,
+        'seconds2': str(seconds).zfill(2),
+        'minutes': minutes,
+        'minutes2': str(minutes).zfill(2),
+        'hours': hours,
+        'hours2': str(hours).zfill(2),
+        'days': days,
+        'years': years,
+        'seconds_total': seconds_total,
+        'minutes_total': minutes_total,
+        'hours_total': hours_total,
+        'days_total': days_total,
+        'years_total': years_total,
+    })
+    

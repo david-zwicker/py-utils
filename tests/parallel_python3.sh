1045cb6d923c74f102c992e0bf6022b94806723c
zwicker <zwicker@10.252.205.176>
2017-02-07 16:04:24 -0500

    Minor updates

--- tests/parallel_python3.sh
+++ tests/parallel_python3.sh
@@ -4,4 +4,4 @@ CORES=`python -c 'from multiprocessing import cpu_count; print(cpu_count())'`
 
 echo 'Run unittests on '$CORES' cores...'
 cd ..
-nosetests-3.4 --processes=$CORES --process-timeout=60 --stop
+nosetests-3.6 --processes=$CORES --process-timeout=60 --stop

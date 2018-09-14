111ec5c0a6fd28052b3d84087f2c1b9435210cb5
david-zwicker <dzwicker@g.harvard.edu>
2018-09-14 20:08:56 +0200

    Adjusted number of cores used in parallel testing

--- tests/parallel_python3.sh
+++ tests/parallel_python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CORES=`python -c 'from multiprocessing import cpu_count; print(cpu_count())'`
+CORES=`python -c 'from multiprocessing import cpu_count; print(cpu_count() // 2)'`
 
 echo 'Run unittests on '$CORES' cores...'
 cd ..

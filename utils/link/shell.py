c00b3a3e0906b3ff21e7824c1e009828ffd31e0f
David Zwicker <david.zwicker@ds.mpg.de>
2019-12-03 09:09:18 +0100

    Added run_command_over_shh

--- utils/link/shell.py
+++ utils/link/shell.py
@@ -7,7 +7,34 @@ Created on Aug 9, 2016
 from __future__ import division
 
 import pipes
+import subprocess as sp
 
 
 
 shellquote = pipes.quote
+
+
+
+def run_command_over_ssh(host, command, user=None):
+    """ run a command on a remote host, optionally using a username
+    
+    If a password needs to be specified, use `user="USER:PASSWORD"`. Note that
+    this is an unsafe function since it uses `shell=True` in the subprocess
+    call. Never use this function with untrusted input!
+    
+    Returns the stdout and stderr of the remote call.
+    """
+    # build the ssh command
+    args = ['ssh',  # run command
+            '-t']   # allocate pseudo-terminal, to use e.g. cd command
+    if user:
+        args.append(user + '@' + host)
+    else:
+        args.append(host)
+    args.append(command)
+    
+    # run the command 
+    proc = sp.Popen(args, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    stdout_data, stderr_data = proc.communicate()
+    
+    return stdout_data, stderr_data

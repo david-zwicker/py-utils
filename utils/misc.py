d856cd6affe8441ed5cd8d107eacdf95ad1bf8a6
David Zwicker <david.zwicker@ds.mpg.de>
2019-12-16 10:07:25 +0100

    Added hybridmethod decorator

--- utils/misc.py
+++ utils/misc.py
@@ -61,6 +61,40 @@ class classproperty(object):
         
     def __get__(self, obj, owner):
         return self.f(owner)
+
+
+
+class hybridmethod:
+    """
+    descriptor that can be used as a decorator to allow calling a method both
+    as a classmethod and an instance method
+     
+    Adapted from https://stackoverflow.com/a/28238047
+    """
+    
+    def __init__(self, fclass, finstance=None, doc=None):
+        self.fclass = fclass
+        self.finstance = finstance
+        self.__doc__ = doc or fclass.__doc__
+        # support use on abstract base classes
+        self.__isabstractmethod__ = bool(
+            getattr(fclass, '__isabstractmethod__', False)
+        )
+        
+
+    def classmethod(self, fclass):
+        return type(self)(fclass, self.finstance, None)
+
+
+    def instancemethod(self, finstance):
+        return type(self)(self.fclass, finstance, self.__doc__)
+
+
+    def __get__(self, instance, cls):
+        if instance is None or self.finstance is None:
+            # either bound to the class, or no instance method available
+            return self.fclass.__get__(cls, None)
+        return self.finstance.__get__(instance, cls)    
     
     
     
